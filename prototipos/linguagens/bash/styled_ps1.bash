dccedb62534517a5bcb0cb2b16616b5e349fac9a
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2024-05-26 15:51:46 -0300

    ...

--- prototipos/linguagens/bash/styled_ps1.bash
+++ prototipos/linguagens/bash/styled_ps1.bash
@@ -15,25 +15,13 @@ cor13='\033[1;34m' # Bright Blue
 cor14='\033[1;35m' # Bright Purple
 cor15='\033[1;36m' # Bright Aqua
 cor16='\033[1;37m' # White
-
-# Cor de fundo do texto
 corBg1="\033[0;40m" # Black
-# Incompleto
-
-# Resetar cores
 rescor='\033[0m' # Reset
 
-# Armazena o valor original de PS1
 xPS1_DEFAULT="$PS1"
-
-# Estilo 1
-# Cercando os comandos de escape de cor para evitar bugs de formatacao de texto
-# Substituir User por qualquer nome desejado
 bashIcon="\$"
 xPS1_STYLE_1="\n\[$cor3\]«\[$rescor\]\[$cor11\]User\[$rescor\]\[$cor3\]»\[$rescor\] \[$cor7\]\w/ \[$rescor\]\[$corBg1\]\[$cor15\]$bashIcon\[$rescor\] "
-# Testando
 PS1="${xPS1_STYLE_1}"
-
 # Alias
 # Padrao
 alias prefixReset='export PS1="$DEFAULT_PS1"'
